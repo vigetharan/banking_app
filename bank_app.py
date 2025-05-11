b3454efe09ac9d85ab6d6aba18edaa3f69022c27
vigetharan <vigetharan9@gmail.com>
2025-05-11 19:44:50 +0530

    Customer details function added

--- bank_app.py
+++ bank_app.py
@@ -1,27 +1,21 @@
-#importing datetime for get date and time for transaction history.
-from datetime import datetime
-
-import getpass
-#create a Text file as users.txt for keeping Default Username and password
+from datetime import datetime                           #importing datetime for get date and time for transaction history.
+import getpass                                          #create a Text file as users.txt for keeping Default Username and password
 try:
     with open('users.txt','x') as file:
         file.write('Admin::user:admin,pass:1234\n')
 except FileExistsError:
     pass
 
-#create a Text file as defaults.txt for keeping upcoming Customer Id and Account Number.......
-try:
+try:                                                    #create a Text file as defaults.txt for keeping upcoming Customer Id and Account Number.......
     with open('defaults.txt','x') as file:
         file.write('C0001,100001')
 except FileExistsError:
     pass
 
-#function for a welcome note
-def welcome():
+def welcome():                                          #function for a welcome note
     print(f'{'=*'*27}\nWELCOME TO ABCD BANK\n{'=*'*27}')
 
-# function for getting a file into dictioanary
-def getFileAsDic(file):
+def getFileAsDic(file):                                 # function for getting a text file into a dictioanary
     dic={}
     with open(file,'r') as file:
         for line in file:
@@ -32,9 +26,8 @@ def getFileAsDic(file):
                 sub_dic[sub_key]=sub_value
             dic[key] = sub_dic
         return dic
-
-# function for getting a dictioanary into a file 
-def writeDicToFile(dic,to_file):
+ 
+def writeDicToFile(dic,to_file):                        # function for getting a dictioanary into a file
     data=""
     for key,value in dic.items():
         sub_data=""
@@ -46,16 +39,11 @@ def writeDicToFile(dic,to_file):
     with open(to_file,'w') as file:
         file.write(data)
 
-# for Get a valid amount
-def get_amount(type):
-    if type == 'Initial':
-        note='To open an account enter the Amount for initial deposit : '
-    elif type == "Withdraw":
-        note = 'Enter the amount to Withdraw : '
-    elif type == 'Deposit':
-        note = 'Enter the amount to Deposit : '
-    elif type == "Transfer":
-        note = 'Enter the amount for Transfer : '
+def get_amount(type):                                       # for Get a valid amount
+    if type == 'Initial':       note='To open an account enter the Amount for initial deposit : '
+    elif type == "Withdraw":    note = 'Enter the amount to Withdraw : '
+    elif type == 'Deposit':     note = 'Enter the amount to Deposit : '
+    elif type == "Transfer":    note = 'Enter the amount for Transfer : '
     while True:
         try:
             amount = float(input(note))
@@ -65,8 +53,13 @@ def get_amount(type):
                 continue
         except ValueError:
             print("Amount must be in digits only")
-#check account number available in accounts details
-def get_acc_no_as_input():
+
+def get_acc_no(customer_id):                            #function for get account by customer id
+    customers=getFileAsDic('customers.txt')
+    account_num = customers[customer_id]['Account_no']
+    return int(account_num)
+
+def get_acc_no_as_input():                              #check account number available in accounts details
     accounts=getFileAsDic('accounts.txt')
     while True:
         try:
@@ -74,8 +67,8 @@ def get_acc_no_as_input():
             for key in accounts:
                 if key == acc_num:
                     return acc_num
-            print('Account number not available\nEnter the correct one OR Contact bank for create an account...\n')
-        except ValueError:
+            print('\tAccount number is not available or not valid\nEnter the correct one OR Contact bank for create an account...\n')
+        except (KeyError, ValueError):
             print('Account number must be numbers and contains only 6 Digits.\n')
             continue
 
@@ -109,56 +102,59 @@ def deposit(customer_id, deposit_type):
     except UnboundLocalError:
         print(f"\t{'X'*45}\n\t\tCustomer id is invalid, please retry again\n")
         pass
-# For get age, DOB, sex from NIC Number
-def get_Nic():
-    mon=[31,29,31,30,31,30,31,31,30,31,30,31]
-    while True:
-        nic_no=input("nic no").strip()
-        if(len(nic_no)==12):
-            year = int(nic_no[:4])
-            day_part = int(nic_no[4:7])
-        elif len(nic_no)==10:
-            day_part = int(nic_no[2:5])
-            if int(nic_no[:2])>10:
-                year = int('19'+nic_no[:2]) # for born before 2000, not works for born before year of 1910
-            else:   year = int('20'+nic_no[:2]) #for born after 2000
-        else:
-            print("Invalid NIC number...!💔\tNIC must be (12 digits)/(9 digits with an letter)\nPlease Re-enter correct number.\n")
-            continue
-        age = int(datetime.now().year)-int(year)
-        if day_part<366:
-            days=day_part
-            sex = "MALE"
-            break
-        elif (day_part-500)<366:
-            days=day_part-500
-            sex = "FEMALE"
-            break
-        else:
-            print('🤢NIC Number you entered is not valid....\n Please Re-Enter your correct NIC Number.')
-    #checking for get day and month with [month]
-    sum=0
-    for i in range (12):
-        sum=sum+mon[i]
-        if sum>days:
-            month = i+1
-            b_day = days-sum+(mon[i])
-            break
-        elif sum == days:
-            month = i+1
-            b_day = (mon[i])
-            break
-        i=i+1
-    details=[nic_no,sex,age,year,month,b_day]
-    return details
 
-#function for ger customer information
-def get_customer():
+def get_Nic():                                      # For get age, DOB, sex from NIC Number
+    mon=[31,29,31,30,31,30,31,31,30,31,30,31]       #assign days in month for check date_part is which day of the year
+    while True:
+        try:        
+            nic_no=input(f"{'Enter your NIC Number : ':<40}:").strip()
+            if(len(nic_no)==12):
+                year = int(nic_no[:4])
+                day_part = int(nic_no[4:7])
+            elif len(nic_no)==10:
+                day_part = int(nic_no[2:5])
+                if int(nic_no[:2])>10:
+                    year = int('19'+nic_no[:2]) # for born before 2000, not works for born before year of 1910
+                else:   year = int('20'+nic_no[:2]) #for born after 2000
+            else:
+                print("\tInvalid NIC number...!💔\tNIC must be (12 digits)/(9 digits with an letter)\nPlease Re-enter correct number.\n")
+                continue
+            age = int(datetime.now().year)-year
+            if day_part<366:
+                days=day_part
+                sex = "MALE"
+            elif (day_part-500)<366:
+                days=day_part-500
+                sex = "FEMALE"
+            else:
+                print('\t💥NIC Number you entered is not valid....\n Please Re-Enter your correct NIC Number.')
+            sum=0                                                           #checking for get day and month with [month]
+            for i in range (12):
+                sum=sum+mon[i]
+                if sum>days:
+                    month = i+1
+                    b_day = days-sum+(mon[i])
+                    break
+                elif sum == days:
+                    month = i+1
+                    b_day = (mon[i])
+                    break
+                i=i+1
+            details=[nic_no,sex,age,year,month,b_day]
+            return details
+        except (ValueError, TypeError):
+            print("\t💥Invalid NIC number...!💔\tNIC must be (12 digits)/(9 digits with an letter)\nPlease Re-enter correct number.\n")
+            continue
+
+def get_customer():                                 #function for ger customer information
     customer_details={}
     while True:
-        name = input("Enter new Customer's name : ")
-        if not name.isalpha():
-            print("NAME must be in letters only... please re enter")
+        name = input(f"{"Enter new Customer's name  ":<40}:")
+        if len(name) < 3:
+            print("\t💥NAME must be in letters only and minimum 3 letters... please re enter a valid name")
+            continue
+        elif not  name.isalpha():
+            print("\t💥NAME must be in letters only and minimum 3 letters... please re enter a valid name")
             continue
         else:
             break
@@ -167,9 +163,9 @@ def get_customer():
     sex = nic_details[1]    
     age = nic_details[2]    
     dob = f'{nic_details[3]}-{nic_details[4]}-{nic_details[5]}'
-    address = input("Enter new Customer's address : ")
-    username = input("Enter new Customer's username : ")
-    password = input(f"Enter password for username of {username}: ")
+    address = input(f"{"Enter new Customer's address":<40}:")
+    username = input(f"{"Enter new Customer's username ":<40}:")
+    password = input(f"{'Enter password for username of {username}':<40}:")
     customer_details =[name, address, nic_no, age, sex, dob, username, password]
     return customer_details
 
@@ -220,26 +216,31 @@ def check_balance(accountNo):
         if int(key) == int(accountNo):
             balance = float(accounts[key]['balance'])
     print(f'Current Account balance of {accountNo} is : {balance:,.2f}')
-#getting Account Transaction history   
+#getting given Account Transaction history   
 def get_transaction_history_by_acc_no(account_no):
     print(f"{'*'*36}\nTRANSACTION HISTORY OF {account_no}\n{'*'*36}")
-    print(f"{'date':<25}{'type of transaction':<25}{'amount':>20}{'balance':>20}\n{'='*50}")
+    print(f"{'No.':<5}{'date':<25}{'type of transaction':<25}{'amount':>20}{'balance':>20}\n{'='*50}")
     transactions=getFileAsDic('transactions.txt')
+    order_no = 0
     for key,value in transactions.items():
         if str(value['acc_no']) == str(account_no):
+            order_no +=1
             print(f"{key:<25}{value['type']:<25}{float(value['amount']):>20,.2f}{float(value['balance']):>20,.2f}\n")
+        else:   print("No transactions found for given sccount number.\n")
 
-
+#getting transaction history for given date
 def get_transaction_history_by_date(date):
     print(f"TRANSACTION HISTORY FOR {date}\n{'*'*81}")
-    print(f"{'Account No.':<15}{'type of transaction':<25}{'amount':>20}{'balance':>20}")
+    print(f"{'No.':<5}{'Account No.':<15}{'type of transaction':<25}{'amount':>20}{'balance':>20}")
     transactions=getFileAsDic('transactions.txt')
-    found = True
+    order_no = 0
     for key in transactions:
         if key.startswith(date):
-            print(f"{transactions[key]['acc_no']:<15}{transactions[key]['type']:<25}{float(transactions[key]['amount']):>20,.2f}{float(transactions[key]['balance']):>20,.2f}\n")
-    # if not found:
-    #     print("No transactions found in given date.\n")
+            order_no +=1
+            print(f"{order_no:<5}{transactions[key]['acc_no']:<15}{transactions[key]['type']:<25}{float(transactions[key]['amount']):>20,.2f}{float(transactions[key]['balance']):>20,.2f}\n")
+    if order_no == 0:
+        print("No transactions found in given date.\n")
+
 #function for inter bank account transfers
 def transfer_between_accounts(customer_id):
     date_time=datetime.today().replace(microsecond=0)
@@ -252,7 +253,7 @@ def transfer_between_accounts(customer_id):
     while True:
         amount = get_amount("Transfer")
         if amount > float(accounts[account_no]['balance']):
-            print('Amount must be lower than account balance...\nPlease re-Try..\n')
+            print('Amount Exceeded account balance...\n amount must be lower than account balance...\n')
             continue
         else: break
     if account_no in accounts:
@@ -280,10 +281,27 @@ def change_pw(c_id,username):
     print('Password changed successfully.!\nPlease login again :')
     main_menu()             #after password change, call main_menu() for relogin
 
+def view_customer_details(c_id):
+    customer_details = getFileAsDic('customers.txt')
+    while True:
+        try:
+            print(f"\tCUSTOMER DETAILS OF :{c_id}\n{'⁙'*63}")
+            print(f"{'NAME ':<15}:{customer_details[c_id]['Name']:<30}{'NIC No.':<20}{customer_details[c_id]['NIC']}\n")
+            print(f"{'GENDER ':<15}:{customer_details[c_id]['sex']:<30}{'DATE OF BIRTH : ':<20}{customer_details[c_id]['dob']}\n")
+            print(f"{'AGE : ':<15}{customer_details[c_id]['Age']:<30}{'ACCOUNT NUMBER : ':<20}{customer_details[c_id]['Account_no']:<20}\n")
+            print(f"{'Adress ':<15}:{customer_details[c_id]['Address']}\n")
+            break  
+        except KeyError:
+            print("\tCUSTOMER ID is not valid\n\tPlease Enter as format of (C****) \n")
+            if input("type 'exit' for out from system....").strip().lower() == 'exit':
+                exit()
+            else:
+                c_id = input("Enter customer id for get details : ")
+
 #MENU FOR PROCESS AS ADMIN, if admin logged in successful.
 def admin_menu():
     while True:
-        print(f"WELCOME TO ABCD BANK as an ADMIN...!!!!\n{'=-'*63}")
+        print(f"\tWELCOME TO ABCD BANK as an ADMIN...!!!!\n{'=-'*63}")
         print('\t\t1. create a customer')
         print('\t\t2. Deposit to an account')
         print("\t\t3. view a customer's details")
@@ -309,7 +327,7 @@ def admin_menu():
 #MENU FOR PROCESS AS USER, if user logged in successful.
 def user_menu():
    while True:
-        print(f"WELCOME TO ABCD BANK AS AN USER...!!!!\n{'=-'*63}")
+        print(f"\tWELCOME TO ABCD BANK AS AN USER...!!!!\n{'=-'*63}")
         print('\t\t1. Check Balance of an account')
         print('\t\t2. Deposit to your account.')
         print('\t\t3. Withdrawal from your account.')
@@ -330,11 +348,6 @@ def user_menu():
         except ValueError:
             print('Invalid input please enter a valid number between 1-9')
             continue
-#function for get account no from user
-def get_acc_no(customer_id):
-    customers=getFileAsDic('customers.txt')
-    account_num = customers[customer_id]['Account_no']
-    return int(account_num)
 
 # Main menu for execution of main program.
 def main_menu():
